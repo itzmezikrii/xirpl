570de11e0944151d6cac7301e09ab6e075cf7aad
itzmezikrii <166141238+itzmezikrii@users.noreply.github.com>
2024-11-29 09:14:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var typed = new Typed('#descText', {
+  strings: [
+    'Study and Grow Together',
+    'Creating a Better Tomorrow',
+    'Unlocking Our True Potential',
+    'Empowered by Knowledge',
+    'Building Our Dreams'
+  ],
+  typeSpeed: 50, 
+  backSpeed: 30, 
+  backDelay: 1000,
+  loop: true, 
+  showCursor: true, 
+  cursorChar: ' |', 
+  startDelay: 500,
+});
+
